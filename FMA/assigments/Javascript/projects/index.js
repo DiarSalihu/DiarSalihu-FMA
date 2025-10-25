7ac7418f426d1f2e9c6b3b95718eb37d2f8c8bd7
DiarSalihu <dsalihu993@gmail.com>
2025-10-25 15:31:00 +0200

    uppdate

---
+++ FMA/assigments/Javascript/projects/index.js
@@ -0,0 +1,6 @@
+
+let allData = [
+  { year: "1998", title: "My Name is Red", cover: "book.jpg", rating: "5.0" },
+  { year: "2021", title: "The Silent Patient", cover: "book3.jpg", rating: "3.2" },
+  { year: "2019", title: "Normal People", cover: "book2.webp", rating: "2.2" }
+];
---
+++ FMA/my-first-react-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>It's the 17th of September 2025 it's 20'C </p>
+    </div>
+  );
+}
+
+export default App;
